f62059f95d316db5d29ebd87d5713f7d75add78d
zzolain <zzolain@gmail.com>
2020-12-28 22:34:37 +0900

    add google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-PGR9E5W5KP`,
       },
     },
     `gatsby-plugin-feed`,
